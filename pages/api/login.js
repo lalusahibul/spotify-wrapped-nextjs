d7203059eed8e641e4e5d9be12e9664d81d78966
LaluSahibul <lalu113ul@gmail.com>
2025-08-25 15:25:29 +0800

    5

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     const scopes = process.env.SPOTIFY_SCOPES;
-    const redirectUri = process.env.SPOTIFY_REDIRECT_URI;
+    const redirectUri = process.env.NEXT_PUBLIC_SPOTIFY_REDIRECT_URI;
     const clientId = process.env.NEXT_PUBLIC_CLIENT_ID;
 
     const authUrl = `https://accounts.spotify.com/authorize?response_type=code&client_id=${clientId}&scope=${encodeURIComponent(
--- pages/callback.js
+++ pages/callback.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
         body: new URLSearchParams({
             grant_type: "authorization_code",
             code: code,
-            redirect_uri: process.env.SPOTIFY_REDIRECT_URI, // HARUS sama dengan yang di dashboard
+            redirect_uri: process.env.NEXT_PUBLIC_SPOTIFY_REDIRECT_URI, // HARUS sama dengan yang di dashboard
         }),
     });
 
